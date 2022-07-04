473e38ed8b01131f5cfde5118bba89d71de5090e
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-04 20:38:49 +0900

    4. mini book

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -3,16 +3,44 @@ import ReactDOM from "react-dom";
 
 // Nested Componentes, React Tools
 
-const Greeting = () => {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+const Author = () => {
+  return <h4>Amelia Hepworth</h4>;
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
