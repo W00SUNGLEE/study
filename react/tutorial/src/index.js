392e183f34953a441b0dd5b95ebecebae29bc2ad
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-04 20:24:44 +0900

    3. Nested Componentes, React Tools

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// JSX Rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
+// Nested Componentes, React Tools
 
 const Greeting = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 };
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => <p>this is my message</p>;
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
