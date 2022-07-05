1d951a8147c5c48a3583fd9c82ff0060bda7f435
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-05 17:31:19 +0900

    7. props

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -3,29 +3,44 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
+// setup vars
+const firstBook = {
+  author: "Amelia Hepworth",
+  title: "I Love You to the Moon and Back",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+};
+const secondBook = {
+  author: "Shannon Olsen and Sandie Sonke",
+  title: "Our Class is a Family",
+  img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
 
-const author = "Amelia Hepworth";
-
-const Book = () => {
-  const title = "I Love You to the Moon and Back";
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      {/*       <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p> */}
     </article>
   );
 };
