0f1bda51974b3c06288fb37e81a95fd1497d6dfa
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-06 00:18:53 +0900

    12. key prop and spread operator

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -6,16 +6,19 @@ import "./index.css";
 // setup vars
 const books = [
   {
+    id: 1,
     author: "Amelia Hepworth",
     title: "I Love You to the Moon and Back",
     img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
   },
   {
+    id: 2,
     author: "Shannon Olsen and Sandie Sonke",
     title: "Our Class is a Family",
     img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
   },
   {
+    id: 3,
     author: "Brit Bennett",
     title: "The Vanishing Half: A Novel",
     img: "https://m.media-amazon.com/images/I/51NJs+Rj6hL.jpg"
@@ -30,15 +33,15 @@ console.log(newNames); */
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book book={book}></Book>;
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 };
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
