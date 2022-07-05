03c6b5309c325c497a9eb9dbba206ed0c53cbb03
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-06 00:03:24 +0900

    11. proper list

--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -1,54 +1,53 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // CSS
 import "./index.css";
 // setup vars
-const firstBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-};
-const secondBook = {
-  author: "Shannon Olsen and Sandie Sonke",
-  title: "Our Class is a Family",
-  img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-};
+const books = [
+  {
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+  },
+  {
+    author: "Shannon Olsen and Sandie Sonke",
+    title: "Our Class is a Family",
+    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+  },
+  {
+    author: "Brit Bennett",
+    title: "The Vanishing Half: A Novel",
+    img: "https://m.media-amazon.com/images/I/51NJs+Rj6hL.jpg"
+  }
+];
 
+/* const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames); */
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim optio
-          mollitia ad eius reiciendis vitae, voluptatem debitis blanditiis ipsa
-          repellat!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
-  console.log(props);
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
 
-ReactDOM.render(<BookList />, document.getElementById("root"));
+// ReactDOM.render(<BookList />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
