613e2ca7ff185f55e44d72f92b4f75dab1492095
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-06 20:18:56 +0900

    14. import and export statements

---
+++ react/tutorial/src/books.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 1,
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+  },
+  {
+    id: 2,
+    author: "Shannon Olsen and Sandie Sonke",
+    title: "Our Class is a Family",
+    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+  },
+  {
+    id: 3,
+    author: "Brit Bennett",
+    title: "The Vanishing Half: A Novel",
+    img: "https://m.media-amazon.com/images/I/51NJs+Rj6hL.jpg"
+  }
+];
--- react/tutorial/src/index.js
+++ react/tutorial/src/index.js
@@ -3,76 +3,19 @@ import ReactDOM from "react-dom/client";
 
 // CSS
 import "./index.css";
-// setup vars
-const books = [
-  {
-    id: 1,
-    author: "Amelia Hepworth",
-    title: "I Love You to the Moon and Back",
-    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-  },
-  {
-    id: 2,
-    author: "Shannon Olsen and Sandie Sonke",
-    title: "Our Class is a Family",
-    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-  },
-  {
-    id: 3,
-    author: "Brit Bennett",
-    title: "The Vanishing Half: A Novel",
-    img: "https://m.media-amazon.com/images/I/51NJs+Rj6hL.jpg"
-  }
-];
 
-/* const names = ["john", "peter", "susan"];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-console.log(newNames); */
+import { data } from "./books";
+import SpecificBook from "./Book";
+import { greeting } from "./testing/testing";
+
 const BookList = () => {
+  console.log(greeting);
   return (
     <section className="booklist">
-      {books.map((book, index) => {
-        return <Book key={book.id} {...book}></Book>;
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
-  );
-};
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference example
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          complexExample(author);
-        }}
-      >
-        more complex example
-      </button>
-    </article>
   );
 };
 
