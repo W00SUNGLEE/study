787007a4e8ed6cca6b62c0a1de7ab9a308e775fc
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-12 23:11:59 +0900

    53. useMemo

--- react/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  }, [url]);
 
   useEffect(() => {
     getProducts();
