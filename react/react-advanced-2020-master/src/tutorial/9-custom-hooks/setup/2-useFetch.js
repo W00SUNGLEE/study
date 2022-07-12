048beb02c729e209e8fd9b9d0b49c565f15d0127
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-07-12 19:17:47 +0900

    43. custom hooks - useFetch

--- react/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react/react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts, url]);
+
+  return { loading, products };
+};
